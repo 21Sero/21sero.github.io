22948076b89abd8a9cf8bdc3c669e1b7b129ae83
Sero <122702052+21Sero@users.noreply.github.com>
2023-03-06 00:10:24 +0400

    Files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from "./container"
+import './App.css'
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header/>
+        <AboutUs/>
+        <SpecialMenu/>
+        <Chef/>
+        <Intro/>
+        <Laurels/>
+        <Gallery/>
+        <FindUs/>
+        <Footer/>
     </div>
   );
 }
